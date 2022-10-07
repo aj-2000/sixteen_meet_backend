98ab03e81ef11a0503ecd7ded79d2b8899bba1d1
adi-coder2000 <contact.quicktech@gmail.com>
2022-10-08 04:19:45 +0530

    Jwt added to Auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { PrismaModule } from './../prisma/prisma.module';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import {JwtModule} from '@nestjs/jwt';
 
-@Module({imports: [PrismaModule], controllers: [AuthController], providers: [AuthService] })
+@Module({imports: [PrismaModule, JwtModule.register({})], controllers: [AuthController], providers: [AuthService] })
 export class AuthModule {}
---
+++ src/user/user.controller.ts
@@ -0,0 +1,22 @@
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { EditUserDto } from './dto/edit-user.dto';
+import { UserService } from './user.service';
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  constructor(private UserService: UserService) {}
+
+  @Get('me')
+  getMe(@GetUser() user: User) {
+    return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: number, @Body() dto: EditUserDto) {
+    return this.UserService.editUser(userId, dto);
+  }
+}
